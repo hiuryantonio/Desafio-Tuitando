1d862765059fe3760018bae345c479b04f5e5f58
Hiury Gomes <hiury_jp@hotmail.com>
2021-09-28 17:52:29 -0300

    Desafio Tuitando

---
+++ DesafioTuitando/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DesafioTuitando
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string v = Console.ReadLine();
+
+            char[] arr = v.ToCharArray();
+
+            if (arr.Length <= 140)
+                Console.WriteLine("TWEET");
+            else
+                Console.WriteLine("MUTE");
+        }
+    }
+}
